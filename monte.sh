a0081e699d90c1adccf9934d178c85b6c2ee011a
Oliver <ot561@york.ac.uk>
2016-07-05 14:23:21 +0100

    added in ratio of successful/total montecarlo moves

--- monte.sh
+++ monte.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-
+#skip=$1
 python insertpair.py > testmc.xyz
 #mpirun -np 6 lammps-daily < in.1dmc
 lammps-daily < in.1dmc
-vmd dumpmc.lammpstrj
-
-
+#if [ $skip -eq 1 ] ; then
+#	vmd dumpmc.lammpstrj
+#else
+#	echo "no skip"
+#fi
