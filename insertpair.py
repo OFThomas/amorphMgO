eabede5a8e8fb681cd913420cfd5b16a2d9ef1cb
Oliver Thomas <ot561@york.ac.uk>
2016-07-07 12:04:19 +0100

    see output.txt in data folder for runs with steps 500 to 5500

--- insertpair.py
+++ insertpair.py
@@ -1,4 +1,8 @@
-#!/bin/usr/env/python
+#!/usr/bin/env/python
+
+#how many atoms in y and z
+ny=6
+nz=6
 
 #lattice constant
 lenunit = 4.212
@@ -15,22 +19,19 @@ xc2=0.5*lenunit
 yc=0
 zc=0
 
-#how many atoms in y and z
-ny=6
-nz=6
-
 #total atoms
 atoms= ny*nz*2
+#for output in .xyz format
 print atoms
 print "0"
 
 for i in range (0, ny):
 	for j in range (0, nz):	
-		#to alternate which column is written
+		#to alternate which column is written, Mg-O or O-Mg
 		if ((i+j)%2 == 0):
 			pair=1
 		else:
-			pair =2
+			pair=2
 		#swap elements for columns#
 		if (pair==1):
 			element1=mg
@@ -38,7 +39,9 @@ for i in range (0, ny):
 		else:
 			element1=o
 			element2=mg
+		#1st element is bottom of column
 		print element1, xc1, yc+(delx*i), zc+(dely*j)
+		#2nd element is on top of column
 		print element2, xc2, yc+(delx*i), zc+(dely*j)
 	
 
--- monte.sh
+++ monte.sh
@@ -2,7 +2,11 @@
 #skip=$1
 python insertpair.py > testmc.xyz
 #mpirun -np 6 lammps-daily < in.1dmc
-lammps-daily < in.1dmc
+lammps-daily < in.1dmc >temp.txt
+tail -n 14 temp.txt >> ./data/output.txt
+#rm ./temp.txt
+grep "final energy" ./data/output.txt
+#mv output.txt ./data/output.txt
 #if [ $skip -eq 1 ] ; then
 #	vmd dumpmc.lammpstrj
 #else
